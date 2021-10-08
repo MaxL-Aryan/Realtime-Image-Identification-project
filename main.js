2275ff98b05b0a552a2d6802af998dc333525042
MaxL-Aryan <79088145+MaxL-Aryan@users.noreply.github.com>
2021-10-08 19:45:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup()
+{
+    canvas=createCanvas(380,300);
+    video=createCapture(VIDEO);
+    video.hide();
+
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+}
+
+function draw()
+{
+    image(video,0,0,380,300);
+
+    classifier.classify(video, gotResult);
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function gotResult(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    }else
+    {
+        console.log(results);
+        document.getElementById("result_object_nme").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
